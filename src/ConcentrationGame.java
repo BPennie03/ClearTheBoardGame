d0d8e5b77f66e90a6a2197f5ddb7306f5772e7bf
BPennie03 <bpennie03@gmail.com>
2022-03-10 22:18:24 -0500

    made epic progress on this epic program

--- src/ConcentrationGame.java
+++ src/ConcentrationGame.java
@@ -1,17 +1,10 @@
 import javax.sound.sampled.*;
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.net.JarURLConnection;
 import java.util.ArrayList;
 
 public class ConcentrationGame {
@@ -27,51 +20,31 @@ public class ConcentrationGame {
         JPanel grid = new JPanel();
         JPanel gameInfo = new JPanel();
         JPanel instructionPanel = new JPanel();
+        JPanel options = new JPanel();
+        JPanel buttonPanel = new JPanel();
 
         JLabel title = new JLabel("Welcome to Clear the Board!");
-        JLabel instructions = new JLabel();
+        JLabel instructions = createLabel("<html> Clear the board is simple, all you have to do is clear the board! Click" +
+                "on a button to remove it, and remove all the buttons as fast as you can! The faster the time, " +
+                "the better the score. Good Luck! </html>");
+        JLabel choicesLabel = createLabel("<html> Please choose from one of the options below to begin: </html>");
 
         title.setHorizontalAlignment(title.CENTER);
         title.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
 
-        // <html> tags were added to allow the JLabel to have text wrapping
-        instructions.setText("<html> Clear the board is simple, all you have to do is clear the board! Click" +
-                "on a button to remove it, and remove all the buttons as fast as you can! The faster the time, " +
-                "the better the score. Good Luck! </html>");
-        instructions.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
-
-        JButton closeInstructions = new JButton("Close Instructions");
-        JButton help = new JButton("Instructions");
-        JButton muteMusic = new JButton("Mute Music");
-
-        JSlider slider = new JSlider();
-
         BorderLayout layout = new BorderLayout();
 
-        // button on instruction screen to close the instruction page
-        closeInstructions.setSize(100, 100);
-        closeInstructions.addActionListener(e -> {
-            instructionFrame.setVisible(false);
-        });
+        JButton fourByFour = createButtons("4x4");
+        JButton fiveByFive = createButtons("5x5");
+        JButton sixBySix = createButtons("6x6");
+        JButton tenByTen = createButtons("10x10");
 
-        // Button at bottom of game screen to allow instructions to be re-opened
-        help.addActionListener(e -> {
-            instructionFrame.setVisible(true);
-        });
-
-        //slider.addChangeListener(this);
-        slider.setPaintTicks(true);
-        slider.setPaintTrack(true);
-        slider.setMajorTickSpacing(10);
-        slider.setPaintLabels(true);
-
-        grid.setLayout(new GridLayout(10, 10));
         grid.setBorder(new EmptyBorder(10, 10, 10, 10));
 
         frame.add(grid, BorderLayout.CENTER);
         frame.add(gameInfo, BorderLayout.SOUTH);
         frame.setSize(750, 650);
-        frame.setVisible(true);
+        frame.setVisible(false);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
@@ -79,37 +52,77 @@ public class ConcentrationGame {
         instructionFrame.add(instructionPanel);
         instructionFrame.setVisible(true);
         instructionFrame.setResizable(false);
-        instructionFrame.setSize(450, 450);
+        instructionFrame.setSize(450, 550);
         instructionFrame.setLocationRelativeTo(null);
 
         instructionPanel.setLayout(layout);
+        instructionPanel.add(buttonPanel, BorderLayout.SOUTH);
         instructionPanel.add(title, BorderLayout.NORTH);
-        instructionPanel.add(closeInstructions, BorderLayout.SOUTH);
         instructionPanel.add(instructions, BorderLayout.CENTER);
-        instructionPanel.add(slider, BorderLayout.SOUTH);
 
-        gameInfo.add(help);
+        buttonPanel.add(options, BorderLayout.SOUTH);
+        buttonPanel.add(choicesLabel, BorderLayout.NORTH);
 
-// *** START OF GAME ***
+        options.setLayout(new GridLayout(1, 4));
+        options.setVisible(true);
+        options.add(choicesLabel, BorderLayout.NORTH);
+        options.add(fourByFour);
+        options.add(fiveByFive);
+        options.add(sixBySix);
+        options.add(tenByTen);
 
+        addActionListener(fourByFour, grid, frame, instructionFrame, 16);
+        addActionListener(fiveByFive, grid, frame, instructionFrame, 25);
+        addActionListener(sixBySix, grid, frame, instructionFrame, 36);
+        addActionListener(tenByTen, grid, frame, instructionFrame, 100);
+
+        // Method to play music in background
+        // playMusic();
+
+    }
+    public JButton createButtons(String label) {
+        JButton button = new JButton(label);
+        button.setSize(100, 50);
+        button.setMaximumSize(new Dimension(100, 50));
+
+        return button;
+    }
+
+    public JLabel createLabel(String text) {
+        JLabel label = new JLabel(text);
+        label.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
+
+        return label;
+    }
+
+    public void addActionListener(JButton button, JPanel grid, JFrame frame, JFrame instructionFrame, int num) {
+        button.addActionListener(e -> {
+            numButtons = num;
+            createGridLayout(grid, frame);
+            instructionFrame.setVisible(false);
+            frame.setVisible(true);
+            createGameButtons(grid);
+        });
+    }
+
+    public void createGridLayout(JPanel grid, JFrame frame) {
+        int length = (int) Math.sqrt(numButtons);
+
+        grid.setLayout(new GridLayout(length, length));
+
+    }
+
+    public void createGameButtons(JPanel grid) {
         // Creates a new arrayList of buttons
         ArrayList<JButton> buttons = new ArrayList<>();
 
-        // Method to play music in background
-        playMusic();
-
-        int randNum = (int)(Math.random() * 5) + 1;
-
-        // Loops x amount of times to populate the game board
-        for (int i=0; i<100; i++) {
+        for (int i=0; i<numButtons; i++) {
             buttons.add(new JButton());
         }
 
-        // Goes through each button in the arrayList and adds it to the grid panel and adds the card image
-        // to the button
         for (JButton button : buttons) {
             grid.add(button);
-            button.setIcon(new ImageIcon("cheatham.png"));
+            button.setIcon(new ImageIcon("reese.png"));
 
             // adds an action listener to each button in the arrayList
             button.addActionListener(e -> {
@@ -121,12 +134,9 @@ public class ConcentrationGame {
                 }
             });
         }
-
     }
 
-
-
-    public static void main(String[] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+    public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
         // Creates a new instance of the ConcentrationGame
         new ConcentrationGame();
 
